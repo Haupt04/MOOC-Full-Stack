ed3a51312ddda3e707dbd0026f68b3beb93e8949
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-05-15 17:43:00 +0200

    Completed 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,33 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from './components/Button'
-import Statistics from './components/Statistics'
+
+
+const Statistics = ({good, neutral, bad}) => {
+  
+  let all = good + neutral + bad
+  let average = (good - bad) / all
+  let percentage = good / all * 100
+
+
+  return (
+    <div>
+        <h1>Statistics</h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <br/>
+        <p>Total Votes: {all}</p>
+
+        <p>Average: { average > 0 ? average.toString() : "0"} </p>
+
+        <p>Positive Percentage: { percentage > 0 ? percentage.toString() + "%": "0%"}</p>
+    </div>
+  )
+}
+
+
+
 
 function App() {
   const [good, setGood] = useState(0)
