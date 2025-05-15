ce8f43aef2ea4a8a22f10d47c875eda59b52942f
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-05-15 17:32:48 +0200

    Completed 1.7

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,11 +1,23 @@
 
 const Statistics = ({good, neutral, bad}) => {
+  
+  let all = good + neutral + bad
+  let average = (good - bad) / all
+  let percentage = good / all * 100
+
+
   return (
     <div>
         <h1>Statistics</h1>
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+        <br/>
+        <p>Total Votes: {all}</p>
+
+        <p>Average: { average > 0 ? average.toString() : "0"} </p>
+
+        <p>Positive Percentage: { percentage > 0 ? percentage.toString() + "%": "0%"}</p>
     </div>
   )
 }
