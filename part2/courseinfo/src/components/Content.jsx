941a8c75c574a2ccaa7e3d58fca7a49ee09c3b5e
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-05-16 12:21:25 +0200

    Completed 2.3

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -2,6 +2,7 @@ import { Part } from "./Part";
 
 
 export const Content = ({ course }) => {
+  // Step 2.3 Done in 2.2 as I already used the reduce method
   const total = course.reduce((sum, amount) => sum + amount.exercises, 0)
  
   return (
