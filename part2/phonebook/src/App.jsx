1cf7bda4ad2d49dd7700c62e0446803a10a37b95
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-05-16 13:34:04 +0200

    Completed 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [show,setShow] = useState(true)
+
+  const handleNewName = (event) => {
+      console.log(event.target.value)
+      setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName("")
+  }
+
+  const listPeople = show ? persons : "No person add yet";
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          Name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {listPeople.map(person => 
+        <p key={person.name}>{person.name}</p>
+      )}
+    </div>
+  )
+}
+
+export default App
