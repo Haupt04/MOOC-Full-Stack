d7c7ef365a16f7292ad04f20fb752a7d8f807397
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-05-16 12:19:55 +0200

    Completed 2.2

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,10 +1,16 @@
 import { Part } from "./Part";
 
 
-export const Content = ({ course }) => (
+export const Content = ({ course }) => {
+  const total = course.reduce((sum, amount) => sum + amount.exercises, 0)
+ 
+  return (
   <div>
     {course.map((c) => {
       return <Part key={c.id} c={c} />;
     })}
+    <div>
+      <p><b>Total Amount of Exercises: {total.toString()}</b></p>
+    </div>
   </div>
-);
+)}
