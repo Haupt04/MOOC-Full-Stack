41d6f79aeb6729806d2fa737688d4ede6524aeda
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-05-21 13:11:15 +0200

    Completed 3.11

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 morgan.token('body', (request) => {
   return request.method === 'POST' ? JSON.stringify(request.body) : '';
@@ -96,9 +103,14 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end()
 })
 
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+console.log('Serving index file from:', path.resolve(__dirname, 'dist', 'index.html'));
