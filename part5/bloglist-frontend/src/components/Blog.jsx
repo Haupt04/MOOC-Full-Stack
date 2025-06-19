33a121a69934b603a7883b0940debd8b5b5cff81
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-06-19 15:44:48 +0200

    Completed 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,11 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
+import { useState } from "react"
+import Togglable from "./Togglable"
+
+const Blog = ({ blog }) => {
+  const [displayVisible, setDisplayVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    paddingBottom: 10
+  }
+
+  const hideWhenVisible = { display: displayVisible? 'none' : '' }
+  const showWhenVisible = { display: displayVisible ? '' : 'none' }
+
+
+
+  return (
+  <div style={blogStyle}>
     <p>{blog.title} by {blog.author}</p>
-     {/* {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )} */}
-      
+    <div style={hideWhenVisible}>
+      <button onClick={() => setDisplayVisible(true)}>View</button>
+    </div>
+
+    <div style={showWhenVisible}>
+      <p>Url: {blog.url}</p>
+      <div>
+        <p>Likes: {blog.likes.toString()}</p>
+        <button>Likes</button>
+      </div>
+      <p>Author: {blog.author}</p>
+      <button onClick={() => setDisplayVisible(false)}>Hide</button>
+    </div>
   </div>  
 )
-
+}
 export default Blog
